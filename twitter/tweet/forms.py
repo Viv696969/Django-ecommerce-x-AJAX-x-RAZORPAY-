c143cb6834b545940719fbce0ed889d070669b23
Vivek Chouhan <113634813+Viv696969@users.noreply.github.com>
2023-03-26 14:48:07 +0530

    Delete twitter directory

--- twitter/tweet/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import * 
-class Tweetform(forms.ModelForm):
-    body=forms.CharField(widget=forms.widgets.Textarea(
-        attrs={'placeholder':'ENTER YOUR TWWET','class':'form-control'}
-    ))
-    class Meta:
-        model=Tweet
-        exclude=['user']
